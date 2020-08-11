32c1d79037a6699025b507bcb5749b176258fdcb
panda_zdwan <panda_zdwan@hotmail.com>
2020-08-12 00:00:19 +0800

    ut:任务本地集成数据落地

--- com.pd.springboot/src/main/java/com/pd/common/util/FileUtil.java
+++ com.pd.springboot/src/main/java/com/pd/common/util/FileUtil.java
@@ -1,12 +1,38 @@
 package com.pd.common.util;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileUtil {
 
 	public static String readAll(File file) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void writeOrOverwrite(String str, String filePath, String fileName) {
+		System.out.println(str);
+		FileWriter fw = null;
+		try {
+			File dir = new File(filePath);
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			File file = new File(filePath + fileName);
+			fw = new FileWriter(file, false);
+			fw.append(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
---
+++ com.pd.springboot/src/test/java/com/pd/springboot/business/SariBusinessTest.java
@@ -0,0 +1,25 @@
+package com.pd.springboot.business;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.pd.businessobject.MapVO;
+import com.pd.common.util.FileUtil;
+import com.pd.common.util.StringX;
+import com.pd.common.util.WebUtil;
+
+public class SariBusinessTest {
+
+	@Test
+	public void test() {
+		String httpStr = WebUtil.post("https://www.zhihu.com/special/19681091/trends", null);
+		MapVO vo = new MapVO();
+		vo.put("type", "sari");
+		vo.put("creationDate", new Date());
+		vo.put("parseBean", "html");
+		vo.put("value", httpStr);
+		FileUtil.writeOrOverwrite(httpStr, "E:/bigdata/saridaily/", StringX.date(new Date(), "yyyyMMdd") + ".txt");
+	}
+
+}
